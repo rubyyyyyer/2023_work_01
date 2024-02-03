c547f61e00ba0ed0e943985cb08c1ba3a9e6bca2
Rubyyyyyer_PC <rubylin0405@.gmail.com>
2024-02-03 12:42:17 +0800

    box kotlin

---
+++ src/main/kotlin/box/Size.kt
@@ -0,0 +1,35 @@
+package box
+
+open class Size(val height: Float, val width: Float, val length: Float) {
+    var boxSize: MutableList<Float> = mutableListOf()
+    open fun reorder(): MutableList<Float> {
+        boxSize = mutableListOf(height, width, length)
+        boxSize.sort()
+        return boxSize
+    }
+
+    fun checkBox(userInput: UserPackage, regularSize: RegularSize): Int {
+
+        var count: Int = 0;
+
+//        println("裡面高度:${height},裡面寬度:${width},裡面長度:${length}")
+//        println("AB高度:${regularSize.height},AB寬度:${regularSize.width},AB長度:${regularSize.length}")
+//        println("輸入高度:${userInput.height},輸入寬度:${userInput.width},輸入長度:${userInput.length}")
+
+        if (userInput.height <= regularSize.height) {
+            count++
+        }
+        if (userInput.width <= regularSize.width) {
+            count++
+        }
+        if (userInput.length <= regularSize.length) {
+            count++
+        }
+        return count
+    }
+}
+
+class UserPackage(height: Float, width: Float, length: Float) : Size(height, width, length)
+class RegularSize(height: Float, width: Float, length: Float) : Size(height, width, length)
+
+
--- src/main/kotlin/box/Tester.kt
+++ src/main/kotlin/box/Tester.kt
@@ -1,16 +1,48 @@
 package box
 
-import box.kotlin.RegularSize
+import java.util.Scanner
 
-class Tester {
-}
-fun main(){
+
+fun main() {
     //Box obj
     //A:長23      寬14      高13 (cm)
     //B:長39.5    寬27.5    高23 (cm)
+    val userInput = userInput()
+    val userInputNum = UserPackage(userInput[0], userInput[1], userInput[2])
+    val regularSizeA = RegularSize(23f, 14f, 13f)
+    val regularSizeB = RegularSize(39.5f, 27.5f, 23f)
 
-    val specifiedSizeA = RegularSize(23f,14f,13f)
-    val specifiedSizeB = RegularSize(39.5f,27.5f,23f)
-    println(specifiedSizeA)
-    println(specifiedSizeB)
+    var checkAReList = regularSizeA.reorder()
+    var checkAPoint = regularSizeA.checkBox(userInputNum, regularSizeA)
+
+    var checkBReList = regularSizeB.reorder()
+    var checkBPoint = regularSizeB.checkBox(userInputNum, regularSizeB)
+
+    println("checkAList:$checkAReList")
+    println("checkBList:$checkBReList")
+    println("checkAList:$checkAPoint")
+    println("checkBList:$checkBPoint")
+
+    if (checkAPoint == 3 && checkBPoint == 3) {
+        println("放在A盒子,比較省錢")
+    } else if (checkAPoint < 3 && checkBPoint < 3) {
+        println("沒有盒子可以放")
+    } else if (checkAPoint == 3) {
+        println("放在A盒子")
+    } else if (checkBPoint == 3) {
+        println("放在B盒子")
+    }
 }
+
+fun userInput(): MutableList<Float> {
+    val scanner = Scanner(System.`in`)
+    print("請用空白鍵間隔輸入長、寬、高:")
+    val userInput = scanner.nextLine()
+    val userInputString: MutableList<String> = userInput.split(" ").toMutableList()
+    val userInputNum: MutableList<Float> = mutableListOf()
+    for (item in userInputString) {
+        userInputNum.add(item.toFloat())
+    }
+    return userInputNum
+}
+
